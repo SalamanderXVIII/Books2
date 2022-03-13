29ed51edb4dfa20d35f091624c672a89e6765ec8
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2022-03-13 14:10:04 +0200

    Добавьте файлы проекта.

---
+++ Books2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public enum Theme
+    {
+        Romance, Fantasy, Horror, Science
+    }
+    public partial class MainWindow : Window
+    {
+        Themes theme = new Themes();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Horror_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Author, book's number, theme." + '\n';
+            for (int i = 0; i < 20; i++)
+            {
+                if (theme.Theme_str[i] == Convert.ToString (Theme.Horror))
+                Catalog.Text += theme.BookAuthors[i] + ", " + Convert.ToString(i + 1) + ", " + theme.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Author, book's number, theme." + '\n';
+            for (int i = 0; i < 20; i++)
+            {
+                Catalog.Text += theme.BookAuthors[i] + ", " + Convert.ToString(i+1) + ", " + theme.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Fantasy_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Author, book's number, theme." + '\n';
+            for (int i = 0; i < 20; i++)
+            {
+                if (theme.Theme_str[i] == Convert.ToString(Theme.Fantasy))
+                    Catalog.Text += theme.BookAuthors[i] + ", " + Convert.ToString(i + 1) + ", " + theme.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Science_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Author, book's number, theme." + '\n';
+            for (int i = 0; i < 20; i++)
+            {
+                if (theme.Theme_str[i] == Convert.ToString(Theme.Science))
+                    Catalog.Text += theme.BookAuthors[i] + ", " + Convert.ToString(i + 1) + ", " + theme.Theme_str[i] + "." + '\n';
+            }
+        }
+
+        private void Romance_Checked(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Author, book's number, theme." + '\n';
+            for (int i = 0; i < 20; i++)
+            {
+                if (theme.Theme_str[i] == Convert.ToString(Theme.Romance))
+                    Catalog.Text += theme.BookAuthors[i] + ", " + Convert.ToString(i + 1) + ", " + theme.Theme_str[i] + "." + '\n';
+            }
+        }
+    }
+}
---
+++ Books2/Themes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books2
+{
+    public class Themes
+    {
+        public int n;
+        public string[] BookAuthors;
+        public string[] Author = { "Christie", "Defoe", "Dickens", "Galsworthy", "Shakespeare" };
+        public string[] Theme_str;
+        public Theme theme;
+        public Random random;
+        public Themes()
+        {
+            n = 20;
+            BookAuthors = new string[n];
+            Theme_str = new string[n];
+            random = new Random();
+            int k = 0;
+            for (int i = 0; i < Author.Length; i++)
+            {
+                for (int j = k; j < k+4; j++)
+                {
+                    BookAuthors[j] = Author[i];
+                }
+                k += 4;
+            }
+            int themename;
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(4);
+                Theme theme = (Theme)themename;
+                Theme_str[i] = Convert.ToString(theme);
+            }
+        }
+    }
+}
